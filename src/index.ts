4662ae7834aaa76353209a5d15988d542fd26e27
Mehdi Eftekhari <me.golden2008@gmail.com>
2023-11-20 08:04:21 +0330

    initialize library configuration

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// export { default as MyButton } from "@/components/MyButton.vue";
+// export { default as Loader } from "@/components/loader/Loader.vue";
+// export { default as BModal } from "@/components/modal/BModal.vue";
+// export * from "@/provider.service";
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,35 @@
-import { defineConfig } from 'vite';
-import { fileURLToPath, URL } from 'node:url';
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import dts from 'vite-plugin-dts'
+import vue from '@vitejs/plugin-vue'
+
+import pkg from './package.json' assert { type: 'json' }
 
 export default defineConfig({
+  plugins: [
+    vue(),
+    dts(),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: "HoonamLib",
+      fileName: 'index',
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys(pkg.dependencies), // don't bundle dependencies
+        /^node:.*/, // don't bundle built-in Node.js modules (use protocol imports!)
+      ],
+    },
+    target: "esnext",
+  },
   resolve: {
     alias: {
       '~': fileURLToPath(new URL('.', import.meta.url)),
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-    }
-  }
+    },
+  },
 });
