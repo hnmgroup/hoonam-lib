47b6515146ff66444bea96fd510abc290acb1668
Mehdi Eftekhari <me.golden.work@gmail.com>
2024-07-22 18:15:38 +0330

    demos added

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,4 @@
 <template>
-  <div style="width: 300px; margin: 0 auto;">
-    <date-time-picker
-      v-model="date"
-      :auto-submit="false"
-      :clearable="clearable"
-      :icon-button="iconButton"
-      :editable="false"
-    ></date-time-picker>
-
-    <div>
-      <input type="checkbox" v-model="clearable" id="clearable">
-      <label for="clearable">Clearable</label>
-    </div>
-
-    <div>
-      <input type="checkbox" v-model="iconButton" id="icon-button">
-      <label for="icon-button">Icon Button</label>
-    </div>
-
-    <div>
-      <input type="checkbox" :checked="rtl" id="rtl"
-             @change="changeDir">
-      <label for="rtl">RTL</label>
-    </div>
-
-    <hr/>
-    <div>
-      {{date?.toString() ?? 'NULL'}}
-    </div>
-  </div>
+  <div><a href="#">Date Time Picker Demo</a></div>
+  <div><a href="#">Forms Demo</a></div>
 </template>
-
-<script setup lang="ts">
-import {DateTimePicker} from "@/components/date-time-picker";
-import {computed, ref} from "vue";
-
-const rtl = computed(() => document.documentElement.dir == 'rtl');
-const iconButton = ref(true);
-const clearable = ref(true);
-const date = ref(new Date());
-
-function changeDir(): void {
-  document.documentElement.dir = document.documentElement.dir == 'rtl' ? 'ltr' : 'rtl'
-}
-</script>
