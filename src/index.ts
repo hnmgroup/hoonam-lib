062588fa985b587dd247288057a863dd45e372bb
Mehdi Eftekhari <me.golden2008@gmail.com>
2024-07-27 06:49:30 +0330

    some issues fixed

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as Provider from "@/provider.service";
+import * as Provider from "@/provider";
 
 export { Provider };
 // export { default as Loader } from "@/components/loader/Loader.vue";
